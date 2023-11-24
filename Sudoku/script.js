bc4ea337e85a216eb059d70f45d521b06bed65a0
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-11-24 10:52:27 +0300

    Anolog clock project, start.

--- Sudoku/script.js
+++ Sudoku/script.js
@@ -7,12 +7,41 @@ for(let i = 0; i < 81; i++){
 }
 
 const btns = document.querySelectorAll(".btn");
+const randomIndexs = [];
+const controlRow =  [
+  [0,8],
+  [9,17],
+  [18,26],
+  [27,35],
+  [36,44],
+  [45,53],
+  [54,62],
+  [63,71],
+  [72,80]
+]
 
-btns.forEach((btn, index) => {
-    btn.addEventListener("click", (e) => {
+window.addEventListener("load", () => {    // 30 tane random index ürettik
+   for(let i = 0; i < 30; i++){
+    const randomIndex =  Math.floor(Math.random() * 82);
+    randomIndexs.push(randomIndex);
+   }
+
+   btns.forEach((btn, index) => {   
+       
+    if(randomIndexs.includes(index))
+    {
         
-    })
+        const randomBtn = Math.ceil(Math.random() * 9)
+        
+        console.log(controlRow[8][1]);
+        if(1){
+            btn.textContent = randomBtn;
+        }
+        
+    }
+   })
 })
+
 
 
 
