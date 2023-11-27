0b7a73fedc3c171eb79718b2c8f23ddc7778e89e
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-11-27 10:55:50 +0300

    Number guess game project, finished.

---
+++ Number_Guessing_Game/script.js
@@ -0,0 +1,48 @@
+const guess = document.querySelector(".text-guess"),
+    input = document.querySelector(".boxs .input"),
+    check = document.querySelector(".boxs .check"),
+    textChances = document.querySelector(".text-chances"),
+    chances = document.querySelector(".text-chances .number-chances");
+
+let randomNumber;
+let inputValue;
+let claim = 10;
+
+window.addEventListener("load", () => {
+    const number = Math.ceil(Math.random() * 100);
+    randomNumber = number;
+    input.focus();
+});
+
+
+input.addEventListener("input", (e) => {
+    let value = e.target.value;
+    inputValue = value;
+})
+
+check.addEventListener("click", () => {
+    if(!(inputValue > 0 && inputValue < 101)){
+        guess.classList.add("danger");
+        guess.textContent = "Your number is invalid";
+        return
+    }else{
+        guess.classList.remove("danger");
+    }
+    claim--;
+    chances.textContent = claim;
+    if(claim === 0){
+        textChances.classList.add("game-over");
+        textChances.innerHTML = "Game Over";
+    }
+    
+    inputValue > randomNumber ? 
+        guess.innerHTML = "you number is high" : 
+        guess.innerHTML =  "you number is low";
+
+    if(randomNumber == inputValue){
+        textChances.innerHTML = "You win..."
+        return;
+    }
+    
+
+})
